c4f96ef7b91589cfa938f613ef8962e7dfc2f732
SHIMAOKA DAIKI <sdaiki@SHIMAOKAnoMac-mini.local>
2025-09-12 11:36:40 +0900

    modify: amplify.yml

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,16 +1,10 @@
 import { defineBackend } from "@aws-amplify/backend";
-import * as cognito from "aws-cdk-lib/aws-cognito";
-import {
-	ArnPrincipal,
-	Effect,
-	Policy,
-	PolicyStatement,
-} from "aws-cdk-lib/aws-iam";
-import { Bucket } from "aws-cdk-lib/aws-s3";
 
-import { env } from "@/lib/env.js";
-import managedLoginSettings from "./auth/managedLoginBrandingSettings.json";
 import { auth } from "./auth/resource.js";
+import { configureCognitoPolicies } from "./config/cognitoPolicies.js";
+import { configureManagedLoginBranding } from "./config/managedLoginBranding.js";
+import { configureSeedBucket } from "./config/seedBucket.js";
+import { configureYarnCraftImageBucket } from "./config/yarnCraftImageBucket.js";
 import { data } from "./data/resource.js";
 import {
 	knittingPatternStorage,
@@ -23,99 +17,8 @@ const backend = defineBackend({
 	knittingPatternStorage,
 	yarnCraftImageStorage,
 });
-const adminRole = backend.auth.resources.groups.admin.role;
 
-// 編み図バケットの登録
-const seedBucket = Bucket.fromBucketAttributes(backend.stack, "seedBucket", {
-	bucketArn: env.SEED_BUCKET_ARN,
-	region: env.SEED_BUCKET_REGION,
-});
-
-seedBucket.grantRead(adminRole);
-
-backend.addOutput({
-	storage: {
-		buckets: [
-			{
-				bucket_name: seedBucket.bucketName,
-				aws_region: seedBucket.env.region,
-				name: "seedBucket",
-			},
-		],
-	},
-});
-
-// Cognitoユーザープールへのアクセス権付与
-const cognitoUserPoolListPolicy = new Policy(
-	backend.auth.stack,
-	"cognitoUserPoolListPolicy",
-	{
-		policyName: "cognitoUserPoolListPolicy",
-		statements: [
-			new PolicyStatement({
-				actions: ["cognito-idp:ListUsers", "cognito-idp:DescribeUserPool"],
-				resources: [backend.auth.resources.userPool.userPoolArn],
-			}),
-		],
-	},
-);
-adminRole.attachInlinePolicy(cognitoUserPoolListPolicy);
-
-// 編み図作品画像へのpublic　access許可、admin roleにput権限付与
-const cfnYarnCraftImageBucket =
-	backend.yarnCraftImageStorage.resources.cfnResources.cfnBucket;
-cfnYarnCraftImageBucket.addPropertyOverride(
-	"PublicAccessBlockConfiguration.BlockPublicPolicy",
-	false,
-);
-cfnYarnCraftImageBucket.addPropertyOverride(
-	"PublicAccessBlockConfiguration.RestrictPublicBuckets",
-	false,
-);
-
-const yarnCraftImageBucket = backend.yarnCraftImageStorage.resources.bucket;
-yarnCraftImageBucket.grantPublicAccess("yarnCraftImage/*");
-yarnCraftImageBucket.addToResourcePolicy(
-	//grantPutだと CloudformationStackCircularDependencyError のため
-	new PolicyStatement({
-		effect: Effect.ALLOW,
-		principals: [new ArnPrincipal(adminRole.roleArn)],
-		actions: ["s3:PutObject"],
-		resources: [`${yarnCraftImageBucket.bucketArn}/yarnCraftImage/*`],
-	}),
-);
-
-// TODO: カスタムドメイン追加時に、マネージドログインを有効化
-// TODO: 検証メッセージ(メールテンプレート)を日本語化
-new cognito.CfnManagedLoginBranding(
-	backend.auth.stack,
-	"KnittingBelleManagedLoginBranding",
-	{
-		userPoolId: backend.auth.resources.userPool.userPoolId,
-		clientId: backend.auth.resources.userPoolClient.userPoolClientId,
-		settings: managedLoginSettings,
-	},
-);
-
-// テスト用adminユーザーにCreateUser権限付与
-const testAdminRole = backend.auth.resources.groups.testAdmin.role;
-
-const cognitoUserPoolCreateUserPolicy = new Policy(
-	backend.auth.stack,
-	"cognitoUserPoolCreateUserPolicy",
-	{
-		policyName: "cognitoUserPoolCreateUserPolicy",
-		statements: [
-			new PolicyStatement({
-				actions: [
-					"cognito-idp:AdminCreateUser",
-					"cognito-idp:AdminSetUserPassword",
-					"cognito-idp:AdminDeleteUser",
-					"cognito-idp:ListUsers",
-				],
-				resources: [backend.auth.resources.userPool.userPoolArn],
-			}),
-		],
-	},
-);
-testAdminRole.attachInlinePolicy(cognitoUserPoolCreateUserPolicy);
+configureSeedBucket(backend);
+configureCognitoPolicies(backend);
+configureYarnCraftImageBucket(backend);
+configureManagedLoginBranding(backend);
